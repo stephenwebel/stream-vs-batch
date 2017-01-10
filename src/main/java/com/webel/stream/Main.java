f6358549819ef6cf00af45489e90524b07e6edb4
swebel <webel@galatea-associates.com>
2017-01-10 15:57:51 -0500

    created logging setup and simple mains

---
+++ src/main/java/com/webel/stream/Main.java
@@ -0,0 +1,13 @@
+package com.webel.stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by swebel on 1/10/2017.
+ */
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Starting Stream Processing");
+    }
+}
